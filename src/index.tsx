cb273f7d41736eeb6bde3f9896e070e060629517
soft-coded <shrutanten.work@gmail.com>
2023-01-27 02:03:16 +0530

    Add boilerplate packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.scss";
-import App from "./App";
+// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
@@ -10,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>{/* <App /> */}</BrowserRouter>
 	</React.StrictMode>
 );
 
