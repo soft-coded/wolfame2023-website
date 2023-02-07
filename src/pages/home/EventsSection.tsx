29e152ac886649a09f0ae46862bd005756eb3421
soft-coded <shrutanten.work@gmail.com>
2023-02-07 12:38:29 +0530

    Finish events section

--- src/pages/home/EventsSection.tsx
+++ src/pages/home/EventsSection.tsx
@@ -1,19 +1,25 @@
 import { useLayoutEffect, useRef } from "react";
 import gsap from "gsap";
+import { Swiper as SwiperClass } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow } from "swiper";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
 
 import slideshow1 from "../../assets/slideshow1.jpg";
-// import slideshow2 from "../../assets/slideshow2.jpg";
-// import slideshow3 from "../../assets/slideshow3.jpg";
-// import slideshow4 from "../../assets/slideshow4.jpg";
-// import slideshow5 from "../../assets/slideshow5.jpg";
-// import slideshow6 from "../../assets/slideshow6.jpg";
-// import slideshow7 from "../../assets/slideshow7.jpg";
-// import slideshow8 from "../../assets/slideshow8.jpg";
-// import slideshow9 from "../../assets/slideshow9.jpg";
-// import slideshow10 from "../../assets/slideshow10.jpg";
-// import slideshow11 from "../../assets/slideshow11.jpg";
-// import slideshow12 from "../../assets/slideshow12.jpg";
-// import slideshow13 from "../../assets/slideshow13.jpg";
+import slideshow2 from "../../assets/slideshow2.jpg";
+import slideshow3 from "../../assets/slideshow3.jpg";
+import slideshow4 from "../../assets/slideshow4.jpg";
+import slideshow5 from "../../assets/slideshow5.jpg";
+import slideshow6 from "../../assets/slideshow6.jpg";
+import slideshow7 from "../../assets/slideshow7.jpg";
+import slideshow8 from "../../assets/slideshow8.jpg";
+import slideshow9 from "../../assets/slideshow9.jpg";
+import slideshow10 from "../../assets/slideshow10.jpg";
+import slideshow11 from "../../assets/slideshow11.jpg";
+import slideshow12 from "../../assets/slideshow12.jpg";
+import slideshow13 from "../../assets/slideshow13.jpg";
 // import slideshow14 from "../../assets/slideshow14.jpg";
 // import slideshow15 from "../../assets/slideshow15.jpg";
 // import slideshow16 from "../../assets/slideshow16.jpg";
@@ -21,55 +27,35 @@ import slideshow1 from "../../assets/slideshow1.jpg";
 // import slideshow18 from "../../assets/slideshow18.jpg";
 
 const eventsList = [
-	{ event: "Futsal" },
-	{ event: "Volleyball" },
-	{ event: "Tug of war" },
-	{ event: "Badminton" },
-	{ event: "Basketball" },
-	{ event: "Chess" },
-	{ event: "Carrom" },
-	{ event: "Table tennis" },
-	{ event: "BGMI" },
-	{ event: "Cricket" },
-	{ event: "Powerlifting" },
-	{ event: "Valorant" },
-	{ event: "Fifa" },
+	{ event: "Futsal", image: slideshow1, color: "#000" },
+	{ event: "Tug of war", image: slideshow3, color: "green" },
+	{ event: "Volleyball", image: slideshow2, color: "var(--clr-primary-dark)" },
+	{ event: "Badminton", image: slideshow4, color: "pink" },
+	{ event: "Basketball", image: slideshow5, color: "violet" },
+	{ event: "Chess", image: slideshow6, color: "purple" },
+	{ event: "Carrom", image: slideshow7, color: "orange" },
+	{ event: "Table tennis", image: slideshow8, color: "green" },
+	{ event: "BGMI", image: slideshow9, color: "red" },
+	{ event: "Cricket", image: slideshow10, color: "yellow" },
+	{ event: "Powerlifting", image: slideshow11, color: "red" },
+	{ event: "Valorant", image: slideshow12, color: "red" },
+	{ event: "Fifa", image: slideshow13, color: "red" },
 ];
+
+function handleSlideChange(swiper: SwiperClass) {
+	const swiperContainer = document.querySelector<HTMLDivElement>(
+		".events-swiper-container"
+	);
+	if (!swiperContainer) return;
+
+	swiperContainer.style.backgroundColor = eventsList[swiper.activeIndex].color;
+}
 
 export default function EventsSection() {
 	const eventsSectionRef = useRef<HTMLElement>(null);
 
 	useLayoutEffect(() => {
-		const ctx = gsap.context(() => {
-			const eventList =
-				eventsSectionRef.current!.querySelector<HTMLElement>(".event-list");
-			const eventImage =
-				eventsSectionRef.current!.querySelector<HTMLElement>(".event-image");
-
-			// event list bg fade animation
-			gsap.to(eventList, {
-				scrollTrigger: {
-					trigger: eventList,
-					start: "top center+=20%",
-					toggleActions: "restart none none reverse",
-				},
-				backgroundColor: "rgb(140, 0, 0)",
-				ease: "power4.out",
-				duration: 0.7,
-			});
-
-			// event-image pin animation
-			gsap.to(".event-image", {
-				scrollTrigger: {
-					trigger: ".event-image",
-					start: "top top+=30px",
-					pin: true,
-					end: `bottom+=${
-						eventList!.clientHeight - eventImage!.clientHeight - 45
-					}px bottom`,
-				},
-			});
-		}, eventsSectionRef);
+		const ctx = gsap.context(() => {}, eventsSectionRef);
 
 		return () => ctx.revert();
 	}, []);
@@ -79,16 +65,35 @@ export default function EventsSection() {
 			<header className="h-screen w-screen flex items-center justify-center">
 				<h1 className="text-center">EVENTS</h1>
 			</header>
-			<ul className="event-list relative">
-				{eventsList.map((eventObj, i) => (
-					<li key={i} className="container">
-						{eventObj.event}
-					</li>
-				))}
-				<li className="event-image image-container absolute">
-					<img src={slideshow1} alt="Event" />
-				</li>
-			</ul>
+			<div className="events-swiper-container h-screen flex items-center justify-center">
+				<Swiper
+					effect="coverflow"
+					grabCursor
+					centeredSlides
+					slidesPerView="auto"
+					// coverflowEffect={{
+					// 	rotate: 50,
+					// 	stretch: 0,
+					// 	depth: 300,
+					// 	modifier: 1,
+					// 	slideShadows: true,
+					// }}
+					// spaceBetween={50}
+					modules={[EffectCoverflow]}
+					className="events-swiper"
+					onSlideChange={handleSlideChange}
+				>
+					{eventsList.map((eventObj, i) => (
+						<SwiperSlide
+							key={i}
+							className="event-slide image-container relative"
+						>
+							<img src={eventObj.image} alt={eventObj.event} />
+							<h1>{eventObj.event}</h1>
+						</SwiperSlide>
+					))}
+				</Swiper>
+			</div>
 		</section>
 	);
 }
