e3afad8b91f75f27b8575bfe4f35606b618149a9
soft-coded <shrutanten.work@gmail.com>
2023-02-19 00:39:52 +0530

    Improve hero-loader

--- src/pages/home/HeroLoader.tsx
+++ src/pages/home/HeroLoader.tsx
@@ -1,9 +1,35 @@
-import LoadingSpinner from "../../components/loading-spinner";
+import { useEffect, useState } from "react";
 
-export default function HeroLoader() {
+export default function HeroLoader({
+	heroLoadingAnim,
+}: {
+	heroLoadingAnim: () => void;
+}) {
+	const [loadedPercent, setLoadedPercent] = useState(0);
+
+	useEffect(() => {
+		const allImages = document.querySelectorAll("img");
+		let haveLoaded = 0;
+
+		function incrementOnLoad() {
+			haveLoaded++;
+			setLoadedPercent(Math.floor(haveLoaded / allImages.length) * 100);
+			if (haveLoaded === allImages.length) {
+				heroLoadingAnim();
+			}
+		}
+
+		allImages.forEach((image) => {
+			image.addEventListener("load", incrementOnLoad, { once: true });
+		});
+	}, [heroLoadingAnim]);
+
 	return (
 		<section className="hero-loader will-change-transform fixed inset-0">
-			<LoadingSpinner className="hero-loader-spinner absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 scale-[2]" />
+			<div className="hero-loader-text w-full h-full flex items-center justify-center flex-col">
+				<h1 className="WOLFAME"></h1>
+				<h3>{loadedPercent}%</h3>
+			</div>
 		</section>
 	);
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useRef } from "react";
+import React, { useRef } from "react";
 import gsap from "gsap";
 
 import HeroLoader from "./HeroLoader";
@@ -9,13 +9,10 @@ import GallerySection from "./GallerySection";
 import Footer from "../../components/footer";
 import "./home.scss";
 
-function heroLoadingAnim(
-	timelineRef: React.MutableRefObject<gsap.core.Timeline | undefined>
-) {
-	timelineRef.current && timelineRef.current.progress(0).kill();
+function heroLoadingAnim() {
 	// make the logo and title ready for the intro anim
-	gsap.set(".logo", { scale: 1.5, opacity: 0 });
-	gsap.set(".title", { opacity: 0 });
+	gsap.set(".hero-section .logo", { scale: 1.5, opacity: 0 });
+	gsap.set(".hero-section .title", { opacity: 0 });
 
 	// pause the slideshow animations
 	const slideShows = document.querySelectorAll<HTMLDivElement>(".slideshow");
@@ -26,25 +23,32 @@ function heroLoadingAnim(
 	const introAnimTimeline = gsap.timeline({
 		defaults: { duration: 1, ease: "power3.out" },
 	});
-	timelineRef.current = introAnimTimeline;
 
-	// hide the hero-loader-spinner
-	introAnimTimeline.to(".hero-loader-spinner", {
+	// hide the hero-loader-text
+	introAnimTimeline.to(".hero-loader-text", {
 		opacity: 0,
 		display: "none",
-		duration: 2,
+		duration: 1,
+		delay: 1,
 	});
 	// hide hero-loader
 	introAnimTimeline.to(".hero-loader", { scaleY: 0, display: "none" });
 	// reveal the logo
-	introAnimTimeline.to(".logo", {
+	introAnimTimeline.to(".hero-section .logo", {
 		opacity: 1,
 		scale: 1,
 	});
 	// reveal the title and play the slideshows
-	introAnimTimeline.to(".title", {
+	introAnimTimeline.to(".hero-section .title", {
 		opacity: 1,
+		x: 0,
 		onComplete: () => {
+			// re-enable scrolling
+			document.documentElement.style.overflowY = "auto";
+			const body = document.getElementsByTagName("body")[0];
+			body.style.overflowY = "auto";
+
+			// play the slideshow animation
 			slideShows.forEach(
 				(slideshow) => (slideshow.style.animationPlayState = "running")
 			);
@@ -54,19 +58,10 @@ function heroLoadingAnim(
 
 export default function HomePage() {
 	const homepageRef = useRef<HTMLDivElement>(null);
-	const timelineRef = useRef<gsap.core.Timeline>();
-
-	useLayoutEffect(() => {
-		const ctx = gsap.context(() => {
-			heroLoadingAnim(timelineRef);
-		}, homepageRef);
-
-		return () => ctx.revert();
-	}, []);
 
 	return (
 		<div ref={homepageRef} className="homepage">
-			<HeroLoader />
+			<HeroLoader heroLoadingAnim={heroLoadingAnim} />
 			<HeroSection />
 			<AboutSection />
 			<EventsSection />
