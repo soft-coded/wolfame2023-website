bc1efee63a932eb9f93a67660c01011c81215953
soft-coded <shrutanten.work@gmail.com>
2023-02-23 09:32:44 +0530

    Add safari gyro

--- src/pages/home/HeroLoader.tsx
+++ src/pages/home/HeroLoader.tsx
@@ -1,4 +1,49 @@
 import { useEffect, useState } from "react";
+import constants from "../../utils/constants";
+import { imageTiltGyro } from "../../utils/functions";
+
+const isSafari =
+	typeof (DeviceOrientationEvent as any).requestPermission === "function";
+
+function GyroInfo({ heroLoadingAnim }: { heroLoadingAnim: () => void }) {
+	function handleSafariGyro() {
+		(DeviceOrientationEvent as any)
+			.requestPermission()
+			.then((res: "granted" | "denied") => {
+				if (res === "granted") {
+					window.addEventListener("deviceorientation", (e) => {
+						imageTiltGyro(e, ".logo");
+						imageTiltGyro(e, ".gallery-image");
+					});
+				} else alert("Gyroscope permission denied");
+			})
+			.catch((err: Error) => alert(err))
+			.finally(() => {
+				heroLoadingAnim();
+			});
+	}
+
+	useEffect(() => {
+		if (isSafari) return;
+
+		setTimeout(() => {
+			heroLoadingAnim();
+		}, 500);
+		/* eslint-disable-next-line */
+	}, []);
+
+	return (
+		<div>
+			{isSafari ? (
+				<button onClick={handleSafariGyro} className="primary-button">
+					ENABLE GYROSCOPE
+				</button>
+			) : (
+				<h6>GYROSCOPE ENABLED</h6>
+			)}
+		</div>
+	);
+}
 
 export default function HeroLoader({
 	heroLoadingAnim,
@@ -6,6 +51,7 @@ export default function HeroLoader({
 	heroLoadingAnim: () => void;
 }) {
 	const [loadedPercent, setLoadedPercent] = useState(0);
+	const [showInfo, setShowInfo] = useState(false);
 
 	useEffect(() => {
 		const allImages = document.querySelectorAll("img");
@@ -16,7 +62,9 @@ export default function HeroLoader({
 			const loadedRatio = haveLoaded / allImages.length;
 			setLoadedPercent(Math.round(loadedRatio * 100));
 			if (haveLoaded === allImages.length) {
-				heroLoadingAnim();
+				if (constants.isMobile) {
+					setShowInfo(true);
+				} else heroLoadingAnim();
 			}
 		}
 
@@ -30,7 +78,11 @@ export default function HeroLoader({
 			<div className="hero-loader-text w-full h-full flex items-center justify-center flex-col">
 				{/* eslint-disable-next-line */}
 				<h1 className="WOLFAME"></h1>
-				<h4>{loadedPercent}%</h4>
+				{showInfo ? (
+					<GyroInfo heroLoadingAnim={heroLoadingAnim} />
+				) : (
+					<h4>{loadedPercent}%</h4>
+				)}
 			</div>
 		</section>
 	);
