127e6a0fba19f480088722ba1f70d10b42fadd75
soft-coded <shrutanten.work@gmail.com>
2023-03-07 05:14:16 +0530

    Finish timeline

--- src/pages/events/TimelineSection.tsx
+++ src/pages/events/TimelineSection.tsx
@@ -5,23 +5,77 @@ import "./events.scss";
 import { animateTitleBg, scrollSnapAnim } from "../../utils/functions";
 import scratchPng from "../../assets/scratch.png";
 
+const timepoints = [
+	{
+		title: "INAUGURATION DAY",
+		date: "15/03/2023",
+		events: ["INITIATION", "INAUGURATION CEREMONY", "FRIENDLY MATCHES"],
+	},
+	{
+		title: "FIRST DAY",
+		date: "16/03/2023",
+		events: [
+			"HUSTLE BEGINS",
+			"ROUND ONE MATCHES",
+			"LEAGUE AND KNOCKOUT STAGES",
+		],
+	},
+	{
+		title: "SECOND DAY",
+		date: "17/03/2023",
+		events: [
+			"MATCHES PROCEED TO THE NEXT ROUND",
+			"MORE INTENSE ACTION",
+			"QUARTER AND SEMI FINALS",
+		],
+	},
+	{
+		title: "THIRD DAY",
+		date: "18/03/2023",
+		events: [
+			"FINALE GRIND",
+			"AWARD CEREMONY",
+			"DECLARATION OF CHAMPION OF CHAMPIONS",
+		],
+	},
+	{
+		title: "CULTURAL DAY",
+		date: "19/03/2023",
+		events: ["CULTURAL NIGHT", "DANCE PARTY", "UNLIMITED FUN"],
+	},
+];
+
 export default function TimelineSection() {
 	const timelineSectionRef = useRef<HTMLElement>(null);
 
 	useLayoutEffect(() => {
+		window.scrollTo(0, 0);
+
 		const ctx = gsap.context(() => {
 			scrollSnapAnim(".timepoint");
-			/* ScrollTrigger.create({
-				trigger: ".timepoints-container",
-				start: "top bottom",
-				markers: true,
-				snap: {
-					snapTo: 1 / timepoints.length,
-					delay: 0,
-					duration: 1,
-				},
-				end: "bottom bottom",
-			}); */
+
+			const timepointNodes = gsap.utils.toArray<HTMLDivElement>(".timepoint");
+			timepointNodes.forEach((node) => {
+				const childNodes = node.querySelectorAll(".container > *");
+
+				gsap.set(childNodes, {
+					y: 50,
+					opacity: 0,
+				});
+
+				gsap.to(childNodes, {
+					scrollTrigger: {
+						trigger: node,
+						start: "top top+=10%",
+						end: "bottom top+=10%",
+						toggleActions: "restart none none reverse",
+					},
+					y: 0,
+					opacity: 1,
+					stagger: 0.2,
+					ease: "power3",
+				});
+			});
 
 			animateTitleBg(".title-bg", "header");
 		}, timelineSectionRef);
@@ -40,18 +94,24 @@ export default function TimelineSection() {
 				</h3>
 			</header>
 			<div className="timepoints-container">
-				<div className="timepoint background-image timepoint-1">
-					Timepoint 1
-				</div>
-				<div className="timepoint background-image timepoint-2">
-					Timepoint 2
-				</div>
-				<div className="timepoint background-image timepoint-3">
-					Timepoint 3
-				</div>
-				<div className="timepoint background-image timepoint-4">
-					Timepoint 4
-				</div>
+				{timepoints.map((timepoint, i) => (
+					<div
+						key={i}
+						className={`timepoint background-image timepoint-${i} overflow-hidden`}
+					>
+						<div className="container flex flex-col items-center justify-center gap-4 h-full overflow-hidden">
+							<h4 className="text-center">{timepoint.title}</h4>
+							<h5 className="text-center font-semibold">{timepoint.date}</h5>
+							<ul className="flex flex-col mt-2 gap-2">
+								{timepoint.events.map((event, j) => (
+									<li key={i + j} className="text-center">
+										- {event}
+									</li>
+								))}
+							</ul>
+						</div>
+					</div>
+				))}
 			</div>
 		</section>
 	);
