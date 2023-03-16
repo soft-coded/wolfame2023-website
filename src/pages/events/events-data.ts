7050858ef6bea5a4f387768d8d4c864e31a4f733
soft-coded <shrutanten.work@gmail.com>
2023-03-16 14:31:18 +0530

    Add alumni info

--- src/pages/events/events-data.ts
+++ src/pages/events/events-data.ts
@@ -32,6 +32,7 @@ const eventsData: EventsDataType = {
 			"All interested students are required to report to their respective hall/hostel JMCR.",
 			"Each participant must have a valid identity card issued by the institution.",
 			"2nd and 1st year participants must have a valid Provisional Admission Letter.",
+			"Alumni can participate from their respective halls.",
 			"Day scholars and PG students can register by contacting the JMCR of the following hall/hostel:<br />1st year: Hostel 8, 9, 10<br />2nd year: Hostel 7, 11, 14<br />3rd year: Hostel 15, 16, Wolfenden Hall, MacDonald Hall, Richardson Hall, Sengupta Hall, Sen Hall<br />4th year: Wolfenden Hall, MacDonald Hall, Richardson Hall, Sengupta Hall, Sen Hall<br /> PG, PhD students: PG Hostel (13)",
 		],
 		photo: hallPhoto,
